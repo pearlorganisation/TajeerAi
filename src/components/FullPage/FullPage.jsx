d8258d17c628b3a6214f16fd8d4ec282b7f587c0
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-04-24 17:24:53 +0530

    full page animation

--- src/components/FullPage/FullPage.jsx
+++ src/components/FullPage/FullPage.jsx
@@ -17,30 +17,59 @@ const FullPage = () => {
     <div className="w-full overflow-hidden">
 
       <div className="flex h-screen w-full snap-x snap-mandatory overflow-hidden scroll-smooth" ref={scrollRef}>
-        <div className="flex h-full w-full flex-shrink-0 snap-start items-center justify-center bg-yellow-100 text-5xl">
-          <div className="w-96">
-            <label for="input-label" class="block text-sm font-medium mb-2">Email1</label>
-            <input onKeyDown={handleKeyDown} type="email" id="input-label" className="py-3 px-4 block w-full rounded-lg focus:outline-none outline-blue-100 text-sm focus:ring-yellow-400/70 focus:ring-4 disabled:opacity-50 disabled:pointer-events-none" placeholder="you@site.com" />
+        <div class=" min-h-screen  px-16 flex h-full w-full flex-shrink-0 snap-start items-center justify-center bg-yellow-100 text-5xl">
+          <div class="relative w-full max-w-lg">
+            <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+            <div class="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+            <div class="m-8 relative space-y-4">
+              <div className="w-96">
+                <label for="input-label" class="block text-sm font-medium mb-2">Email1</label>
+                <input onKeyDown={handleKeyDown} type="email" id="input-label" className="py-3 px-4 block w-full rounded-lg focus:outline-none outline-blue-100 text-sm focus:ring-yellow-400/70 focus:ring-4 disabled:opacity-50 disabled:pointer-events-none" placeholder="you@site.com" />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="flex h-full w-full flex-shrink-0 snap-start items-center justify-center bg-red-100 text-5xl">
-          <div className="w-96">
-            <label for="input-label" class="block text-sm font-medium mb-2">Email2</label>
-            <input onKeyDown={handleKeyDown} type="email" id="input-label" className="py-3 px-4 block w-full rounded-lg focus:outline-none outline-blue-100 text-sm ring-red-400/70 focus:ring-4 disabled:opacity-50 disabled:pointer-events-none" placeholder="you@site.com" />
+        <div class=" min-h-screen  px-16 flex h-full w-full flex-shrink-0 snap-start items-center justify-center bg-red-100 text-5xl">
+          <div class="relative w-full max-w-lg">
+            <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+            <div class="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+            <div class="m-8 relative space-y-4">
+              <div className="w-96">
+                <label for="input-label" class="block text-sm font-medium mb-2">Email2</label>
+                <input onKeyDown={handleKeyDown} type="email" id="input-label" className="py-3 px-4 block w-full rounded-lg focus:outline-none outline-blue-100 text-sm ring-red-400/70 focus:ring-4 disabled:opacity-50 disabled:pointer-events-none" placeholder="you@site.com" />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="flex h-full w-full flex-shrink-0 snap-start items-center justify-center bg-green-100">
-          <div className="w-96">
-            <label for="input-label" class="block text-sm font-medium mb-2">Email3</label>
-            <input onKeyDown={handleKeyDown} type="email" id="input-label" className="py-3 px-4 block w-full rounded-lg focus:outline-none outline-blue-100 text-sm ring-green-400/70 focus:ring-4 disabled:opacity-50 disabled:pointer-events-none" placeholder="you@site.com" />
+        <div class=" min-h-screen  px-16 flex h-full w-full flex-shrink-0 snap-start items-center justify-center bg-green-100">
+          <div class="relative w-full max-w-lg">
+            <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+            <div class="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+            <div class="m-8 relative space-y-4">
+              <div className="w-96">
+                <label for="input-label" class="block text-sm font-medium mb-2">Email3</label>
+                <input onKeyDown={handleKeyDown} type="email" id="input-label" className="py-3 px-4 block w-full rounded-lg focus:outline-none outline-blue-100 text-sm ring-green-400/70 focus:ring-4 disabled:opacity-50 disabled:pointer-events-none" placeholder="you@site.com" />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="flex h-full w-full flex-shrink-0 snap-start items-center justify-center bg-pink-100 text-5xl">
-          <div className="w-96">
-            <label for="input-label" class="block text-sm font-medium mb-2">Email4</label>
-            <input onKeyDown={handleKeyDown} type="email" id="input-label" className="py-3 px-4 block w-full rounded-lg focus:outline-none outline-blue-100 text-sm ring-pink-400/70 focus:ring-4 disabled:opacity-50 disabled:pointer-events-none" placeholder="you@site.com" />
+        <div class=" min-h-screen  px-16 flex h-full w-full flex-shrink-0 snap-start items-center justify-center bg-pink-100 text-5xl">
+          <div class="relative w-full max-w-lg">
+            <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+            <div class="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+            <div class="m-8 relative space-y-4">
+              <div className="w-96">
+                <label for="input-label" class="block text-sm font-medium mb-2">Email4</label>
+                <input onKeyDown={handleKeyDown} type="email" id="input-label" className="py-3 px-4 block w-full rounded-lg focus:outline-none outline-blue-100 text-sm ring-pink-400/70 focus:ring-4 disabled:opacity-50 disabled:pointer-events-none" placeholder="you@site.com" />
+              </div>
+            </div>
           </div>
         </div>
+
       </div>
 
     </div>
